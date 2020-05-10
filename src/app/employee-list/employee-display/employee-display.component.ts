c8bca5c729feb4950c8bc53d43be2c7468086e8a
vidyasagar-mr <vidyasagar.mr15@gmail.com>
2020-05-11 00:10:05 +0530

    modified:   src/app/employee-list/employee-display/employee-display.component.ts

--- src/app/employee-list/employee-display/employee-display.component.ts
+++ src/app/employee-list/employee-display/employee-display.component.ts
@@ -5,41 +5,24 @@ import { Employee } from '../employee.model';
   selector: 'app-employee-display',
   templateUrl: './employee-display.component.html'
 })
-export class EmployeeDisplayComponent {
+export class EmployeeDisplayComponent implements OnInit,OnChanges{
+  //No need of @Input as we are using Angular Content projection using ng-content
+   private employee:Employee;
 
+  @Output() employeeEvent:EventEmitter<Employee>=new EventEmitter<Employee>();
 
-
-  @Output() employeeEvent: EventEmitter<Employee> = new EventEmitter<Employee>();
-
-  //To Track Changes in input property
-
-  //1. using ngOnchanges
-  // @Input() public employee:Employee;
-
-  // ngOnChanges(changes: SimpleChanges): void {
-  //    console.log(changes.employee.previousValue);
-  //    console.log(changes.employee.currentValue);
-  //    console.log(changes.employee.firstChange);
-  //    console.log(changes.employee.isFirstChange);
-  //  }
-
-
-  //2. using property setter
-  private _employee: Employee;
-
-  @Input() set employee(emp: Employee) {
-    this._employee = emp;
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
   }
+ 
+   ngOnInit() {
+   }
+ 
+   onClickingDelete():void{
+     this.employeeEvent.emit(this.employee);
+   }
 
-  get employee(): Employee {
-    return this._employee;
-  }
-
-  onClickingDelete(): void {
-    this.employeeEvent.emit(this.employee);
-  }
-
-
+   
 
 
 
--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -8,19 +8,21 @@ import { EmployeeDisplayComponent } from './employee-display/employee-display.co
 })
 export class EmployeeListComponent implements OnInit{
 
+@ViewChild(EmployeeDisplayComponent,{static:false}) employeeDisplayComponent:EmployeeDisplayComponent;
 
-  constructor() { }
 
-  ngOnInit() {
-  }
-  employee_repo:Employee[]=[{id:123,name:'Nikhil',age:26,photoPath:'https://filmfare.wwmindia.com/content/2020/feb/hrithikroshan41582899194.jpg'},
-  {id:124,name:'Mohan',age:27,photoPath:'https://filmfare.wwmindia.com/content/2020/feb/hrithikroshan41582899194.jpg'}];
+constructor() {}
 
-  onDeleting(emp_delete:Employee):void{
-  let emp_Index:number= this.employee_repo.indexOf(emp_delete);
-  if (emp_Index > -1) {
-  this.employee_repo.splice(emp_Index,1)
-  }
-  }
+ngOnInit() {
+}
+employee_repo:Employee[]=[{id:123,name:'Nikhil',age:26,photoPath:'https://filmfare.wwmindia.com/content/2020/feb/hrithikroshan41582899194.jpg'},
+{id:124,name:'Mohan',age:27,photoPath:'https://filmfare.wwmindia.com/content/2020/feb/hrithikroshan41582899194.jpg'}];
+
+onDeleting(emp_delete:Employee):void{
+let emp_Index:number= this.employee_repo.indexOf(emp_delete);
+if (emp_Index > -1) {
+this.employee_repo.splice(emp_Index,1)
+}
+}
 
 }
