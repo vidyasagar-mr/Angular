93c99657fa91d088223cc3f4464f0eea0a04cfc2
vidyasagar-mr <vidyasagar.mr15@gmail.com>
2020-05-10 14:37:36 +0530

    CustomDirective without using renderer

--- src/app/directive/display-name.directive.ts
+++ src/app/directive/display-name.directive.ts
@@ -1,10 +1,11 @@
-import { Directive, HostListener, Renderer2, ElementRef, OnInit } from '@angular/core';
+import { Directive, HostListener, Renderer2, ElementRef, OnInit, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appDisplayName]'
 })
 export class DisplayNameDirective implements OnInit{
  
+  @HostBinding('style.backgroundColor') backgroundColor:string='transparent';
 
  constructor(private eleRef:ElementRef,private renderer:Renderer2) { }
  
@@ -15,12 +16,23 @@ export class DisplayNameDirective implements OnInit{
 
 //bestPractise to use Renderer as below
   @HostListener('mouseover',['$event'])
-  onMousingOver(eventData:Event){
+  onMousingOverUsingRenderer(eventData:Event){
     this.renderer.setStyle(this.eleRef.nativeElement,'background-color','aqua');
   }
   @HostListener('mouseout',['$event'])
-  onMousingOut(eventData:Event){
+  onMousingOutUsingRenderer(eventData:Event){
     this.renderer.setStyle(this.eleRef.nativeElement,'background-color','transparent');
   }
+
+//Using Render like above is not problem, but we can do above operation with even better way without using renderer
+@HostListener('mouseover',['$event'])
+onMousingOverWithoutUsingRenderer(eventData:Event){
+  this.backgroundColor='aqua';
+}
+@HostListener('mouseout',['$event'])
+onMousingOutWithoutUsingRenderer(eventData:Event){
+  this.backgroundColor='transparent';
+}
+
 
 }
